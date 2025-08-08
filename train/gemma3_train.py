3af1c490de62889b83df4f2d7993608fb38522b8
dasgauranga2 <dasgauranga2@gmail.com>
2025-08-08 11:57:31 +0930

    Comments in training code

--- train/gemma3_train.py
+++ train/gemma3_train.py
@@ -1,40 +1,28 @@
-import io
 from typing import Any, cast
-import requests
 from transformers import AutoProcessor, Gemma3ForConditionalGeneration
-from transformers import TrainingArguments
 from trl import SFTTrainer, SFTConfig
 from peft import LoraConfig
 from datasets import IterableDataset, Features, load_dataset
-import datasets
-from PIL import Image
-import numpy as np
 
-
-def load_image(url):
-    response = requests.get(url)
-    image = Image.open(io.BytesIO(response.content))
-    return image
-
-def image_from_bytes(image_bytes):
-    return Image.open(io.BytesIO(image_bytes))
-
+# model id
 model_id = "google/gemma-3-4b-it"
-
+# load model
 model = Gemma3ForConditionalGeneration.from_pretrained(
     model_id, device_map="auto"
 )
-model.config.use_cache = False  # Disable caching for training
+model.config.use_cache = False  # disable caching for training
 
+# load model processor
 processor = AutoProcessor.from_pretrained(model_id, padding_side="right")
 processor.tokenizer.pad_token = processor.tokenizer.eos_token  # Use eos token as pad token
 processor.tokenizer.padding_side = "right"
 
+# load training data
 train_data = load_dataset("flaviagiammarino/path-vqa", split="train")
-train_data = train_data.select(range(1000))
+#train_data = train_data.select(range(1000))
 print(train_data[0])
 
-
+# format each data sample into a format for gemma 3 processor
 def format_example(example):
     return {
         "images": example["image"],
@@ -54,21 +42,22 @@ def format_example(example):
             }
         ]
     }
-    
+
+# format the dataset
 train_ds = train_data.map(format_example)
 
+# collate function
+# combine list of data points to create a batch of input data
 def collate_fn(examples):
-    # Get the texts and images, and apply the chat template
+    # process the text data
     texts = [processor.apply_chat_template(example["messages"], tokenize=False) for example in examples]
+    # list of images
     images = [example["images"] for example in examples]
 
-    # Tokenize the texts and process the images
+    # process the text and input images
     batch = processor(text=texts, images=images, return_tensors="pt", padding=True)
 
-    # print("collate_fn pixel_values", batch["pixel_values"].shape)
-    # print("collate_fn input_ids", batch["input_ids"].shape)
-
-    # The labels are the input_ids, and we mask the padding tokens in the loss computation
+    # labels are the target outputs
     labels = batch["input_ids"].clone()
     labels[labels == processor.tokenizer.pad_token_id] = -100
     labels[labels == processor.image_token_id] = -100
@@ -76,7 +65,8 @@ def collate_fn(examples):
 
     return batch
 
-# Set up LoRA configuration for causal language modeling
+# setup LoRA config
+# important if GPU resources are limited
 lora_config = LoraConfig(
     r=8,
     lora_alpha=16,
@@ -86,22 +76,26 @@ lora_config = LoraConfig(
     task_type="CAUSAL_LM"
 )
 
-# Define training arguments
+# training configuration
 training_args = SFTConfig(
-    output_dir="./results",
-    num_train_epochs=1,
+    output_dir="checkpoint/",
+    num_train_epochs=4,
     per_device_train_batch_size=1,
+    gradient_accumulation_steps=8,
     learning_rate=2e-4,
-    logging_steps=1,
-    save_steps=25,
+    logging_steps=50,
+    save_strategy="epoch",
+    save_total_limit=1, # keep only the latest one
+    #save_steps=25,
     #report_to="tensorboard",
     group_by_length=False,
     remove_unused_columns=False,
     dataset_kwargs = {"skip_prepare_dataset": True},
+    gradient_checkpointing=True,
     gradient_checkpointing_kwargs = dict(use_reentrant=False),
 )
 
-# Create the SFTTrainer with LoRA parameters
+# trainer class
 trainer = SFTTrainer(
     model=model,
     train_dataset=cast(Any, train_ds),
@@ -111,4 +105,5 @@ trainer = SFTTrainer(
     processing_class=processor.tokenizer,
 )
 
+# start training
 trainer.train()
